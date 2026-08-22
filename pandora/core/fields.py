3f325eca1fc1631c38ec1887fdb6b43a6c5dc671
Daniëlle Huisman <danielle@huisman.me>
2026-08-22 14:01:08 +0200

    fix(core): use `char(32)` as type for `UUID4Field`

--- pandora/core/fields.py
+++ pandora/core/fields.py
@@ -12,3 +12,12 @@ class UUID4Field(UUIDField):
             kwargs['editable'] = False
 
         super().__init__(*args, **kwargs)
+
+    def db_type(self, connection):
+        return "char(32)"
+
+    def get_db_prep_value(self, value, connection, prepared=False):
+        value = super().get_db_prep_value(value, connection, prepared)
+        if value is None:
+            return None
+        return value.hex
